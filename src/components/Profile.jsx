75ae0bd61a9f1a8d14d651b6b1b8da2cbbf24c4a
dami-wabe <damiuse18@gmail.com>
2024-11-09 10:54:08 +0600

    done

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -15,42 +15,83 @@ import { FaTwitter } from "react-icons/fa";
 const Profile = () => {
   return (
     <div>
-      <div className=" flex xl:mt-[120px] mt-[90px] xl:fixed z-10">
+      <div className="flex xl:mt-[120px] mt-[90px] xl:fixed z-10">
         <div className="flex flex-col xl:gap-[40px] gap-5 px-[30px] mt-8 xl:mt-[70px]">
-          <div className=" w-[45px] h-[45px] hover:border-[1px] flex   items-center justify-center rounded-full">
-            <TiHomeOutline className="text-white text-[35px] hover:text-[#F5BD4D] " />
-          </div>
-          <div className="w-[45px] h-[45px] hover:border-[1px] flex  items-center justify-center rounded-full">
-            <MdManageAccounts className="text-white text-[35px] hover:text-[#F5BD4D] " />
-          </div>
-          <div className="w-[45px] h-[45px] hover:border-[1px] flex  items-center justify-center rounded-full">
-            <ImBriefcase className="text-white text-[32px] hover:text-[#F5BD4D] " />
-          </div>
-          <div className="w-[45px] h-[45px] hover:border-[1px] flex  items-center justify-center rounded-full">
-            <LuPenSquare className="text-white text-[32px] hover:text-[#F5BD4D] " />
-          </div>
-          <div className="w-[45px] h-[45px] hover:border-[1px] flex  items-center justify-center rounded-full">
-            <MdPermPhoneMsg className="text-white text-[32px] hover:text-[#F5BD4D] " />
-          </div>
+          <a
+            href="/"
+            className="w-[45px] h-[45px] hover:border-[1px] flex items-center justify-center rounded-full"
+          >
+            <TiHomeOutline className="text-white text-[35px] hover:text-[#F5BD4D]" />
+          </a>
+          <a
+            href="/"
+            className="w-[45px] h-[45px] hover:border-[1px] flex items-center justify-center rounded-full"
+          >
+            <MdManageAccounts className="text-white text-[35px] hover:text-[#F5BD4D]" />
+          </a>
+          <a
+            href="/"
+            className="w-[45px] h-[45px] hover:border-[1px] flex items-center justify-center rounded-full"
+          >
+            <ImBriefcase className="text-white text-[32px] hover:text-[#F5BD4D]" />
+          </a>
+          <a
+            href="/"
+            className="w-[45px] h-[45px] hover:border-[1px] flex items-center justify-center rounded-full"
+          >
+            <LuPenSquare className="text-white text-[32px] hover:text-[#F5BD4D]" />
+          </a>
+          <a
+            href="/"
+            className="w-[45px] h-[45px] hover:border-[1px] flex items-center justify-center rounded-full"
+          >
+            <MdPermPhoneMsg className="text-white text-[32px] hover:text-[#F5BD4D]" />
+          </a>
         </div>
+
         <div className="w-[390px] bg-black pl-5 pb-2">
           <Image src={profileimage} />
           <div className="flex items-center justify-center gap-3">
-            <div className=" w-[32px] h-[32px] bg-[#121414] text-[10px] rounded-full flex items-center justify-center text-white">
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-[32px] h-[32px] bg-[#121414] text-[10px] rounded-full flex items-center justify-center text-white"
+            >
               <TfiFacebook />
-            </div>
-            <div className=" w-[32px] h-[32px] bg-[#121414] text-[10px] rounded-full flex items-center justify-center text-white">
+            </a>
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-[32px] h-[32px] bg-[#121414] text-[10px] rounded-full flex items-center justify-center text-white"
+            >
               <FaLinkedinIn />
-            </div>
-            <div className=" w-[32px] h-[32px] bg-[#121414] text-[10px] rounded-full flex items-center justify-center text-white">
+            </a>
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-[32px] h-[32px] bg-[#121414] text-[10px] rounded-full flex items-center justify-center text-white"
+            >
               <FaPinterestP />
-            </div>
-            <div className=" w-[32px] h-[32px] bg-[#121414] text-[10px] rounded-full flex items-center justify-center text-white">
+            </a>
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-[32px] h-[32px] bg-[#121414] text-[10px] rounded-full flex items-center justify-center text-white"
+            >
               <FaInstagram />
-            </div>
-            <div className=" w-[32px] h-[32px] bg-[#121414] text-[10px] rounded-full flex items-center justify-center text-white">
+            </a>
+            <a
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="w-[32px] h-[32px] bg-[#121414] text-[10px] rounded-full flex items-center justify-center text-white"
+            >
               <FaTwitter />
-            </div>
+            </a>
           </div>
         </div>
       </div>
